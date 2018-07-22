49e8171baf761baf02522abbe3423b6b149b261c
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-22 19:09:25 -0400

    psyuedo, images, bootstrap

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+// set random goal value
+// set random value for the cystals
+// creat buttons that change your score
+//if value equals goal then you win
+// if value passes the goal then you lose
+// have a counter to keep track of every win and lose
+// when a win or loss happens reset the goal value and crystal value
