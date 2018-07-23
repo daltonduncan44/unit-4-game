e11a2a1284952644f21082629fd58fbf5269efbd
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-23 16:53:55 -0400

    start js math floors for stones

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,13 @@
+// set up values
+var win = 0;
+var loss = 0;
+var goalScore;
+
+
+var stone1 = Math.floor(Math.random()*(12 - 1) + 1);
+var stone2 = Math.floor(Math.random()*(12 - 1) + 1);
+var stone3 = Math.floor(Math.random()*(12 - 1) + 1);
+var stone4 = Math.floor(Math.random()*(12 - 1) + 1);
 // set random goal value
 // set random value for the cystals
 // creat buttons that change your score
@@ -5,3 +15,12 @@
 // if value passes the goal then you lose
 // have a counter to keep track of every win and lose
 // when a win or loss happens reset the goal value and crystal value
+
+
+
+
+
+
+
+// first check to see if link works
+console.log("hey this works");
