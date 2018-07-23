5128408a4678eee7305b37af64795555f456359c
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-23 17:24:42 -0400

    rest made for game end

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,16 @@ var loss = 0;
 var goalScore;
 
 
+
+// set random goal value
+// set random value for the cystals
+var randNum = Math.floor(Math.random()* (120-19) + 19);
+console.log(randNum)
 var stone1 = Math.floor(Math.random()*(12 - 1) + 1);
 var stone2 = Math.floor(Math.random()*(12 - 1) + 1);
 var stone3 = Math.floor(Math.random()*(12 - 1) + 1);
 var stone4 = Math.floor(Math.random()*(12 - 1) + 1);
-// set random goal value
-// set random value for the cystals
+console.log(stone4);
 // creat buttons that change your score
 //if value equals goal then you win
 // if value passes the goal then you lose
@@ -20,6 +24,7 @@ var stone4 = Math.floor(Math.random()*(12 - 1) + 1);
 
 
 
+//function reset(){randNum= Math.floor(math.random()* (120-19) + 19);
 
 
 // first check to see if link works
